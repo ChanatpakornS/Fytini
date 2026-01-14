99a747617fd516979e15ba01e3afa12adb9ba4dd
csap <csirintronsopon8@gmail.com>
2026-01-14 14:04:10 +0700

    feat: simple form output no validation

--- web/src/components/LinkForm.tsx
+++ web/src/components/LinkForm.tsx
@@ -1,10 +1,17 @@
 "use client";
 
 import { Button } from "@/components/Button";
+import { useState } from "react";
 
 function LinkForm() {
+  const [url, setUrl] = useState("");
+  const [alias, setAlias] = useState("");
+  const [expiration, setExpiration] = useState("");
+
   function handleSubmit() {
-    alert("Form submitted!");
+    alert(
+      `Form submitted!\n URL: ${url}\n Alias: ${alias}\n Expiration: ${expiration}`,
+    );
   }
 
   return (
@@ -14,14 +21,23 @@ function LinkForm() {
         type="text"
         placeholder="https://example.com"
         className="border border-gray-300 rounded-md px-4 py-2 w-full"
+        onChange={(event) => setUrl(event.target.value)}
       />
       <legend>Custom-alias:</legend>
       <input
         type="text"
         placeholder="fytini"
         className="border border-gray-300 rounded-md px-4 py-2 w-full"
+        onChange={(event) => setAlias(event.target.value)}
       />
-      <div className="text-right">
+      <legend>Expiration:</legend>
+      <input
+        type="datetime-local"
+        placeholder="fytini"
+        className="border border-gray-300 rounded-md px-4 py-2 w-full"
+        onChange={(event) => setExpiration(event.target.value)}
+      />
+      <div className="text-right mt-4">
         <Button type="submit" className="w-40">
           Submit
         </Button>
